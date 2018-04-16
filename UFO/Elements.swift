3624fec9183783d14764b011bdbb4c40bc01be6c
Timmy Le <le.timmy94@gmail.com>
2018-04-16 16:01:53 -0400

    setup physics categories

--- UFO/Elements.swift
+++ UFO/Elements.swift
@@ -1,1 +1,10 @@
+import SpriteKit
 
+struct CollisionBitMask {
+    static let ufoCategory:UInt32 = 0x1 << 0
+    static let wallCategory:UInt32 = 0x1 << 1
+    static let groundCategory:UInt32 = 0x1 << 3
+}
+
+extension GameScene {
+}
