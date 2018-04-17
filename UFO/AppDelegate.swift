24fd40cf4c7402f8904ff2b5297a10f5c6341416
Daniel Freudberg <dbfreudber@gmail.com>
2018-04-17 13:04:11 -0400

    looking at various game tutorials online and trying to figure out what they mean. And adapt them accordingly. Swift is sweet

--- UFO/AppDelegate.swift
+++ UFO/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  UFO
 //
-//  Created by Timmy Le on 4/16/18.
+//  Created by Timmy Le and Daniel Freudberg on 4/16/18.
 //  Copyright © 2018 Red Blue Green. All rights reserved.
 //
 
--- UFO/GameScene.swift
+++ UFO/GameScene.swift
@@ -2,7 +2,32 @@
 //  GameScene.swift
 //  UFO
 //
-//  Created by Timmy Le on 4/16/18.
+//  Created by Timmy Le and Daniel Freudberg on 4/16/18.
 //  Copyright © 2018 Red Blue Green. All rights reserved.
 //
+
+import SpriteKit
+
+class GameScene: SKScene , SKPhysicsContactDelegate {
+
+    var isGameStarted = Bool(false)
+    var isDied = Bool(false)
+    let music = SKAction.playSoundFileNamed("Komiku_-_54_-_Escaping_like_Indiana_Jones.mp3", waitForCompletion: false)
+    
+// when we do scores... add them here
+//start
+    var taptoplayLbl = SKLabelNode()
+//restart
+    var restartBtn = SKSpriteNode()
+// squares need to be involved in spritekit
+    var squares = SKNode()
+// for game movement:
+    var moveAndRemove = SKAction()
+    
+    //create the flying object, adding
+    let flyingObjectAtlas = SKTextureAtlas(named:"player")
+    var flyingObjectSprites = Array()
+    var flyingObject = SKSpriteNode()
+    var repeatActionFlyingObject = SKAction()
+}
 
